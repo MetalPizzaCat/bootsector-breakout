0026beaf06a6a9abc19923a83c3396388f218242
metalpizzacat <catguy228@gmail.com>
2025-06-07 00:05:04 +1000

    add paddle collision

--- main.asm
+++ main.asm
@@ -30,20 +30,69 @@ game_loop:
         dec di
         jnz .loop
 
-update_ball:
+update_paddle:
+    mov bx, [paddle.x]
+    in al, keyboard.port
+    cmp al, keyboard.left
+    je .move_left
+    cmp al, keyboard.right
+    je .move_right
+    jmp .draw
+
+    .move_left:
+        test bx, bx
+        jz .draw
+        dec word [paddle.x]
+        jmp .draw
+
+    .move_right:
+        cmp bx, screen.w - paddle.w
+        jge .draw
+        inc word [paddle.x]
+        
+    .draw:
+        mov ax, bx
+        mov bx, paddle.y
+        mov dl, paddle.color
+        mov cx, paddle.size
+        call draw_rect
+
+
+prepare_ball:
     mov ax, [ball.x]
     mov bl, [ball.y]
+
+; the check is
+; if  ball.x >= paddle.x and ball.x <= paddle.x + paddle.w
+; and ball.y >= paddle.y and ball.y <= paddle.y + paddle.h then
+; negate speed
+; end
+check_paddle_collision:
+    mov cx, [paddle.x]
+    cmp ax, cx                                              ; ball.x >= paddle.x
+    jl .end
+    cmp bl, paddle.y                                        ; ball.y >= paddle.y
+    jl .end
+    add cx, paddle.w                                        ; paddle.x + paddle.w
+    cmp ax, cx                                              ; ball.x <= paddle.x + paddle.w
+    jg .end
+    cmp bl, paddle.y + paddle.h                             ; ball.y <= paddle.y + paddle.h 
+    jg .end
+    neg byte [ball.y_speed]
+    .end:
+
+handle_ball:
     .check_wall_collision_hor:
-        cmp ax, screen.w - ball.w                       ; check if hit right wall(accounting for ball width)
+        cmp ax, screen.w - ball.w                           ; check if hit right wall(accounting for ball width)
         jge .bounce_hor
-        test ax, ax                                     ; check if hit left wall(which would be 0)
+        test ax, ax                                         ; check if hit left wall(which would be 0)
         jnz .check_wall_collision_vert
         .bounce_hor:
             neg word [ball.x_speed]                         ; invert the ball speed
     .check_wall_collision_vert:
-        test bl, bl                                     ; test if hit top of the screen
+        test bl, bl                                         ; test if hit top of the screen
         jnz .draw_ball
-        neg byte [ball.y_speed]                         ; invert the vertical speed
+        neg byte [ball.y_speed]                             ; invert the vertical speed
     .draw_ball:
     xor bh, bh
     mov cx, ball.size
@@ -152,8 +201,12 @@ board:
     .bricks times .total db 0
 
 paddle:
-    .x dw screen.w / 2
+    .x dw screen.w / 2 - .w / 2
     .y equ 180
+    .w equ 35
+    .h equ 5
+    .color equ 15
+    .size equ (.h << 8) | .w
 
 ball:
     .w equ 5
@@ -161,7 +214,7 @@ ball:
     .size equ (.h << 8) | .w
     .color equ 15                                       ; white ball
     .x dw screen.w / 2 - .w / 2                         ; x has to be a word because max screen width is 320 which would not fit into a byte 
-    .y db screen.h - 20                                 ; y
+    .y db screen.h - 30                                 ; y
     .x_speed dw 1
     .y_speed db -1
 
@@ -170,6 +223,12 @@ screen:
     .w equ 320
     .h equ 200
     .frame_delay equ 8192
+
+keyboard:
+    .port equ 0x60
+    .left equ 0x1e                                      ; A key
+    .right equ 0x20                                     ; D key
+    .start equ 0x39                                     ; Space key
 
 
 padding:
